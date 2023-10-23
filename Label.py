f9919011458b70835750a3794b4c2db0a2555125
Muhammad Saad Nomani <107791651+saadnom@users.noreply.github.com>
2023-10-23 20:12:00 +0500

    Add files via upload

---
+++ Label.py
@@ -0,0 +1,5 @@
+from tkinter import *
+root=Tk()
+label=Label(text="Saad Nomani",bg="Blue",fg="White",padx=30,pady=50,font="Algerian 72 bold underline",borderwidth=3,relief=SUNKEN)
+label.pack(side=TOP,anchor='n',fill=Y)
+label.pack(side=RIGHT,fill=Y)
